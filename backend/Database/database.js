720e16b4391cb684aa0f057fd034561612660f51
unknown <mafaigabriel@gmail.com>
2025-03-02 19:34:44 +0100

    db

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -1,12 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'ccec_db',
-    password: 'root',
-    port: 5432,
-});
+    connectionString: process.env.DATABASE_URL, 
+    ssl: {
+      rejectUnauthorized: false, 
+    },
+  });
 
-// Export the pool for use in other files
+
 module.exports = pool;

194b64940823732008ec65e29ab40b45fa15b91c
unknown <mafaigabriel@gmail.com>
2025-03-03 18:29:30 +0100

    asd

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const allowedOrigins = ["https://cc-1-c-xi.vercel.app", "http://localhost:5173"];
+const allowedOrigins = ["https://cc-1-c.vercel.app", "http://localhost:5173"];
 
 app.use(cors({
     origin: function (origin, callback) {

5a8b0bfbd8b9c5956c2fedf2f2fb62cabd5acf8f
unknown <mafaigabriel@gmail.com>
2025-03-03 11:23:48 +0100

    der

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,12 +7,14 @@ import VisionMision from '../../components/Home/VisionMision'
 import ContactForm from '../../components/Home/ContactForm'
 import PartnersSection from '../../components/Home/PartnersSection'
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const Home = () => {
     const [founder, setFounder] = useState(null);
 
     useEffect(() => {
         const roleName = encodeURIComponent("Founder and CEO "); // Encodde spaces
-        fetch(`http://localhost:5000/api/roles/role/${roleName}`)
+        fetch(`${API_BASE_URL}/api/roles/role/${roleName}`)
           .then((response) => response.json())
           .then((data) => {
             console.log("Fetched Founder:", data);
@@ -52,7 +54,7 @@ const Home = () => {
                 {/* Image Section */}
                 <div className="md:w-1/3 flex flex-col items-center text-center">
                     <img
-                    src={`http://localhost:5000${founder.image_path}`} 
+                    src={`${API_BASE_URL}${founder.image_path}`} 
                     alt="Founder"
                     className="w-40 h-40 rounded-full object-cover border-4 border-white shadow-md"
                     />

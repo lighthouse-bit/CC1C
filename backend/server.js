3ec0504ec247136fc27efbc6befca1767c731c10
unknown <mafaigabriel@gmail.com>
2025-04-12 13:42:25 +0100

    jrjrj

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,14 @@ const app = express();
 // const allowedOrigins = ["https://cc-1-c.vercel.app", "http://localhost:5173"];
 
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://cc-1-c-xi.vercel.app'  
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.use(express.json());
